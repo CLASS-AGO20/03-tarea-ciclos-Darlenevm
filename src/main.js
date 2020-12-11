39caa9f82f49e16367f4997506ccd9fa404f2f27
Darlenevm <dmoreno9@ucol.mx>
2020-12-10 20:09:29 -0600

    primo

--- src/main.js
+++ src/main.js
@@ -6,9 +6,38 @@ export default class App {
         }
         return suma;
     }
+    sumatoriaSerieDos(numero) {
+        let suma = 1;
+        for (let i = 2; i <= numero; i++) {
+            if (i % 2 === 0) {
+            suma = suma + 1 / i;
+            } else {
+            suma = suma - 1 / i;
+            }
+        }
+        return suma;
+        }
+    esPrimo(numero){
+        let i;
+        if(numero == 0 | numero == 1 | numero == 4){
+            return false;
+            }
+            for(i=2; i<numero/2; i++){
+                if(numero % i == 0){
+                    return false;
+                }
+            }
+            return true;
+            }
+    
 
 }
 
 let app = new App();
 console.log(app.sumatoriaSerieUno(10));
+console.log("");
+console.log(app.sumatoriaSerieDos(10));
+console.log("");
+console.log(app.esPrimo(3));
+console.log(app.esPrimo(4));
 console.log("");
